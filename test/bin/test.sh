6fd1cd7e703ba2e823a79d73072df65bdb362f02
Brian W. Wolter <brianwolter@gmail.com>
2026-04-06 22:11:47 -0400

    ...

--- test/bin/test.sh
+++ test/bin/test.sh
@@ -44,10 +44,43 @@ echo "agent: $NEXUS_AGENT"
 assert_equal $($NEXUS ticket list | jq -sc 'map(.id) | sort') '[]'
 
 # add two tickets
-$NEXUS ticket new --summary "This is the first ticket we've created"
-$NEXUS ticket new --summary "This is the second ticket we've created"
+actual="$($NEXUS ticket new --summary "This is the first ticket we've created" | jq -scr '.[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+read_eof expect <<EOF
+{"id":1,"summary":"This is the first ticket we've created","owner_id":null,"roles":null}
+EOF
+assert_equal "$expect" "$actual"
+
+actual="$($NEXUS ticket new --summary "This is the second ticket we've created" --role "reviewer" | jq -scr '.[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+read_eof expect <<EOF
+{"id":2,"summary":"This is the second ticket we've created","owner_id":null,"roles":["reviewer"]}
+EOF
+assert_equal "$expect" "$actual"
+
+# list all tickets
+actual="$($NEXUS ticket list | jq -scr 'sort_by(.id) | .[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
 read_eof expect <<EOF
 {"id":1,"summary":"This is the first ticket we've created","owner_id":null,"roles":[]}
-{"id":2,"summary":"This is the second ticket we've created","owner_id":null,"roles":[]}
+{"id":2,"summary":"This is the second ticket we've created","owner_id":null,"roles":["reviewer"]}
 EOF
-assert_equal "$expect" "$($NEXUS ticket list | jq -scr 'sort_by(.id) | .[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+assert_equal "$expect" "$actual"
+
+# list filtered by role
+actual="$($NEXUS ticket list --role reviewer | jq -scr 'sort_by(.id) | .[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+read_eof expect <<EOF
+{"id":2,"summary":"This is the second ticket we've created","owner_id":null,"roles":["reviewer"]}
+EOF
+assert_equal "$expect" "$actual"
+
+# list filtered by multiple roles
+actual="$($NEXUS ticket list --role reviewer --role nonexistent | jq -scr 'sort_by(.id) | .[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+read_eof expect <<EOF
+{"id":2,"summary":"This is the second ticket we've created","owner_id":null,"roles":["reviewer"]}
+EOF
+assert_equal "$expect" "$actual"
+
+# take a ticket
+actual="$($NEXUS ticket take --id 1 | jq -scr '.[] | {id: .id, summary: .summary, owner_id: .owner_id, roles: .roles}')"
+read_eof expect <<EOF
+{"id":1,"summary":"This is the first ticket we've created","owner_id":"${NEXUS_AGENT}","roles":[]}
+EOF
+assert_equal "$expect" "$actual"
